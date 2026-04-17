f977b7beddebb0f56c5defe369b0e487753902a6
risper njeri <rispernjeri789@gmail.com>
2026-04-17 09:07:00 +0300

    feat:add all changes

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 // Write your code here!
-async: function displayPosts(posts) {
-  const ul = document.getElementById("post-list");
-
-  if (!ul) {
-    console.error("post-list not found!");
-    return;
-  }
-
-  ul.innerHTML = ""; // important for tests
-
-  posts.forEach(post => {
-    const li = document.createElement("li");
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
-
-    h1.textContent = post.title;
-    p.textContent = post.body;
-
-    li.appendChild(h1);
-    li.appendChild(p);
-    ul.appendChild(li);
-  });
-}();
-
-async function fetchPosts() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await response.json
-
+async function getSocialPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    
+    console.log(data);
+    
     displayPosts(data);
-  } catch (error) {
-    console.error(error);
-  }
 }
 
+function displayPosts(posts) {
+    const postList = document.querySelector('#post-list');
 
-fetchPosts();
+    posts.forEach(post => {
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1');
+        const p = document.createElement('p');
+
+        h1.textContent = post.title;
+        p.textContent = post.body;
+
+        li.appendChild(h1);
+        li.appendChild(p);
+        
+        postList.appendChild(li);
+    });
+}
+
+getSocialPosts();
